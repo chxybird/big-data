e4b58eff508299dea4fb581b1ffad84a447ed20f
小鸟程序员 <lipu@supconit.com>
2021-12-21 14:34:34 +0800

    大数据调研初始化

---
+++ flume/src/main/java/com/bird/FlumeApp.java
@@ -0,0 +1,16 @@
+package com.bird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 李璞
+ * @Date 2021/12/21 14:31
+ * @Description
+ */
+@SpringBootApplication
+public class FlumeApp {
+    public static void main(String[] args) {
+        SpringApplication.run(FlumeApp.class);
+    }
+}
