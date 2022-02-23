959e020ba358086530be6916c16ccf5cabc9df85
小鸟程序员 <2450107493@qq.com>
2022-02-23 22:14:39 +0800

    flink

--- flink/src/main/java/com/bird/word_count/WordCount.java
+++ flink/src/main/java/com/bird/word_count/WordCount.java
@@ -1,10 +1,12 @@
 package com.bird.word_count;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.operators.AggregateOperator;
 import org.apache.flink.api.java.operators.DataSource;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
 
 /**
  * @Author 李璞
@@ -12,18 +14,26 @@ import org.apache.flink.api.java.tuple.Tuple2;
  * @Description 批处理WordCount
  */
 public class WordCount {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //创建执行环境
         ExecutionEnvironment environment = ExecutionEnvironment.getExecutionEnvironment();
         //加载文件数据
         DataSource<String> dataSource = environment.readTextFile("F:\\log\\input.txt");
         //计算数据
-        AggregateOperator<Tuple2<String, Integer>> sum = dataSource.flatMap((FlatMapFunction<String, Tuple2<String, Integer>>) (s, collector) -> {
+        DataSet<Tuple2<String,Integer>> resultSet = dataSource.flatMap(new MyflatMapper()).groupBy(0).sum(1);
+        //打印结果
+        resultSet.print();
+    }
+
+    static class MyflatMapper implements FlatMapFunction<String,Tuple2<String,Integer>>{
+        @Override
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+            // 按空格分词
             String[] words = s.split(" ");
-            for (String word : words) {
-                Tuple2<String, Integer> tuple2 = new Tuple2<>(word, 1);
-                collector.collect(tuple2);
+            // 遍历所有word，包成二元组输出
+            for (String str : words) {
+                collector.collect(new Tuple2<>(str, 1));
             }
-        }).groupBy(0).sum(1);
+        }
     }
 }
---
+++ netty/src/main/java/com/bird/NettyApp.java
@@ -0,0 +1,16 @@
+package com.bird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 小鸟程序员
+ * @Date 2022/2/5 21:27
+ * @Description
+ */
+@SpringBootApplication
+public class NettyApp {
+    public static void main(String[] args) {
+        SpringApplication.run(NettyApp.class);
+    }
+}
